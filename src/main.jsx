454cf0dd968ff8a644825ceb554c2c2c1a47b005
LarissaSchmidt <larissaschmidt541@gmail.com>
2023-10-10 18:51:33 -0300

    Feat: Criação de pastas usando o Vite

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ListaDeTarefas from './Page/ListaDeTarefas.jsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ListaDeTarefas/>
+  </React.StrictMode>,
+)
