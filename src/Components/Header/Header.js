0103b027cd2c9dbca31185592fbe3efd675c2a61
LarissaSchmidt <larissaschmidt541@gmail.com>
2023-10-11 10:28:37 -0300

    Feat: Inclusão do arquivo Header.js  com import styled-components e estilo para componente

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyleHeader = styled.header`
+    display: flex;
+    flex-direction: column;
+    background-color: #D99ACE;
+    width: 100%;
+    height: 50px;
+    align-items: center;
+    font-size: 200%;
+    font-family: 'Montserrat', sans-serif;
+    font-family: 'Work Sans', sans-serif;
+    justify-content: center;`
