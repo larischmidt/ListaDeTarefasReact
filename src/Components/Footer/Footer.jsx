7440a1e802436888373783d7c37baa66204359ad
LarissaSchmidt <larissaschmidt541@gmail.com>
2023-10-11 10:31:17 -0300

    Feat: Inclusão do arquivo Footer.jsx com importação do React e StyleFooter

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {StyleFooter} from './Footer'
+
+const Footer = () => {
+  return (
+    <StyleFooter>Larissa Schmidt</StyleFooter>
+  )
+}
+
+export default Footer
