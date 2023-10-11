52596716ee4987186c2a4bb9f9787da66be9874d
LarissaSchmidt <larissaschmidt541@gmail.com>
2023-10-11 10:32:02 -0300

    Feat: Inlusão do arquivo Footer.js com a importaçãp styled-components e estilo do arquivo

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+
+export const StyleFooter = styled.footer`
+    display: flex;
+    flex-direction: column;
+    background-color: #D99ACE;
+    width: 100%;
+    height: 50px;
+    align-items: center;
+    font-size: 200%;
+    font-family: 'Montserrat', sans-serif;
+    font-family: 'Work Sans', sans-serif;
+    justify-content: center;`
