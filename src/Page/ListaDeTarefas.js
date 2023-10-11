296033e28b632c1fdcc0b2bb22efdf57c5f2beaf
LarissaSchmidt <larissaschmidt541@gmail.com>
2023-10-11 10:23:17 -0300

    Feat: Inclusão do arquivo ListaDeTarefas com a importação do styled-components e estilo para o arquivo

---
+++ src/Page/ListaDeTarefas.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyleListaDeTarefas = styled.div`
+
+display: flex;
+flex-direction: column;
+min-height: 100vh;
+border:solid red;
+align-items: center;
+
+`
