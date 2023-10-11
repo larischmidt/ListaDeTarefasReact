50a36c38dab5637b7bc9427c8370ef3531ae6782
LarissaSchmidt <larissaschmidt541@gmail.com>
2023-10-11 10:42:16 -0300

    Feat: Inclusão da importação ddo arquivo Body.jsx no arquivo ListaDeTarefas

--- src/Page/ListaDeTarefas.jsx
+++ src/Page/ListaDeTarefas.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../Components/Header/Header.jsx";
 import Footer from "../Components/Footer/Footer.jsx";
+import Body from "../Components/Body/Body.jsx";
 import { StyleListaDeTarefas } from "./ListaDeTarefas.js";
 const ListaDeTarefas = () => {
   return (
     <StyleListaDeTarefas>
-      <Header/>  
+      <Header/> 
+      <Body/> 
       <Footer/>          
     </StyleListaDeTarefas>
   )
