2bfc0fa98de82c66eaa9c568939abb1aa3fd6544
LarissaSchmidt <larissaschmidt541@gmail.com>
2023-10-11 10:43:42 -0300

    Feat:Inclusão da importação dos arquivos GlobalStyle.js e ListaDeTarefas.jsx no aqurivo Main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import ListaDeTarefas from './Page/ListaDeTarefas.jsx'
-
+import { StyleGlobal } from './Styles/GlobalStyle.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <StyleGlobal/>
     <ListaDeTarefas/>
   </React.StrictMode>,
 )
