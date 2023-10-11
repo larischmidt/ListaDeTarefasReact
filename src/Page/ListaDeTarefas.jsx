249c51f431dac2b2478b430a66b906934a395fc4
LarissaSchmidt <larissaschmidt541@gmail.com>
2023-10-11 10:33:06 -0300

    Feat: Inclusão da importação do arquivo Footer.jsx no arquivo ListaDeTarefas

--- src/Page/ListaDeTarefas.jsx
+++ src/Page/ListaDeTarefas.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../Components/Header/Header.jsx";
+import Footer from "../Components/Footer/Footer.jsx";
 import { StyleListaDeTarefas } from "./ListaDeTarefas.js";
 const ListaDeTarefas = () => {
   return (
