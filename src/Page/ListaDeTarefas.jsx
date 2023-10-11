540781c750c1038abc2706a0f1a6692e8f527acd
LarissaSchmidt <larissaschmidt541@gmail.com>
2023-10-11 10:20:41 -0300

    Feat: Inclusão Do arquivo ListaDeTarefas Com a estrutura import React

---
+++ src/Page/ListaDeTarefas.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "../Components/Header/Header.jsx";
+import Footer from "../Components/Footer/Footer.jsx";
+import Body from "../Components/Body/Body.jsx";
+import { StyleListaDeTarefas } from "./ListaDeTarefas.js";
+
+
+
+const ListaDeTarefas = () => {
+  return (
+    <StyleListaDeTarefas>
+      
+      <Header/>
+      <Body/>
+      <Footer/>
+      
+    </StyleListaDeTarefas>
+  )
+}
+
+
+
+export default ListaDeTarefas;
